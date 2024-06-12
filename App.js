5497b3517dbc4081f03cb53b00286174cfedf106
mazherhussian <mazherhussain926@gmail.com>
2024-06-12 07:55:09 +0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect, useRef } from "react";
+import { Text, View, StyleSheet, Image } from "react-native";
+import { Camera, CameraView, useCameraPermissions } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import Button from "./src/components/Button.js";
 
 export default function App() {
+  const [permission, requestPermission] = useCameraPermissions();
+  const [image, setImage] = useState(null);
+  const [facing, setFacing] = useState("back");
+  const [flash, setFlash] = useState("off");
+  const cameraRef = useRef(null);
+
+  // Request camera and media library permissions when the component mounts
+  useEffect(() => {
+    (async () => {
+      try {
+        MediaLibrary.requestPermissionsAsync();
+        const cameraStatus = await Camera.requestCameraPermissionsAsync();
+        requestPermission(cameraStatus.status === "granted");
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+
+  // Function to take a picture using the camera
+  const takePicture = async () => {
+    if (cameraRef) {
+      try {
+        const data = await cameraRef.current.takePictureAsync();
+        console.log(data);
+        setImage(data.uri);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  // Function to save the taken picture to the media library
+  const savePicture = async () => {
+    if (image) {
+      try {
+        await MediaLibrary.createAssetAsync(image);
+        alert("Picture is saved");
+        setImage(null);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
+  // Function to toggle the camera between front and back
+  const toggleCameraFacing = () => {
+    setFacing((current) => (current === "back" ? "front" : "back"));
+  };
+
+  // Function to toggle the flash mode between off and on
+  const toggleFlashMode = () => {
+    setFlash((current) => (current === "off" ? "on" : "off"));
+  };
+
+  // Display a message if camera permissions are not granted
+  if (!permission) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {!image ? (
+        <CameraView
+          style={styles.camera}
+          ref={cameraRef}
+          flash={flash}
+          facing={facing}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              padding: 20,
+             // backgroundColor:"#f1f1f1"
+            }}
+          >
+            <Button toggleIcon="camera-reverse-outline" onPress={toggleCameraFacing} />
+            <Button
+              icon="flash"
+              color={flash === "off" ? "#f1f1f1" : "#ef9c66"}
+              onPress={toggleFlashMode}
+            />
+          </View>
+        </CameraView>
+      ) : (
+        <Image source={{ uri: image }} style={styles.camera} />
+      )}
+      {image ? (
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            paddingHorizontal: 20,
+          }}
+        >
+          <Button
+            title={"Re-take"}
+            icon="retweet"
+            onPress={() => setImage(null)}
+          />
+          <Button title={"Save"} icon="check" onPress={savePicture} />
+        </View>
+      ) : (
+        <View>
+          <Button
+            title={"Take a picture"}
+            icon="camera"
+            onPress={takePicture}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -13,8 +124,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    justifyContent: "center",
+    paddingBottom: 20,
+  },
+  image: {},
+  camera: {
+    flex: 1,
+    borderRadius: 20,
   },
 });
---
+++ src/components/Button.js
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Text, TouchableOpacity, StyleSheet } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+export default function Button({ title, onPress, icon, color, toggleIcon }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      {icon ? (
+        <Entypo name={icon} size={28} color={color ? color : "#f1f1f1"} />
+      ) : (
+        <Ionicons
+          name={toggleIcon}
+          size={28}
+          color={color ? color : "#f1f1f1"}
+        />
+      )}
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+const styles = StyleSheet.create({
+  button: {
+    height: 40,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontWeight: "bold",
+    fontSize: 16,
+    color: "#f1f1f1",
+    marginLeft: 10,
+  },
+});
